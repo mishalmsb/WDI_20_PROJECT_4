f650c09c405ce96bd3c1b2e37a2011152a78d07f
Mishal Shashikant <mishalmsb@gmail.com>
2016-07-13 11:04:23 +0100

    added material dashboard to home

---
+++ public/js/client_backup.js
@@ -0,0 +1,6 @@
+// angular
+//   .module('lifeLine', ['angular-jwt', 'ngResource', 'ui.router', 'ngMaterial', 'ngMdIcons'])
+//   .constant('API', 'http://localhost:3000/api')
+//   .config(function($httpProvider) {
+//     $httpProvider.interceptors.push('authInterceptor');
+//   })
